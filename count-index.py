e4c5643db7ae8ed5a8e118e7a76a456df85d6e67
EIkhan <ebrahimkhan5222@gmail.com>
2020-05-09 02:19:26 +0600

    Added count-index

---
+++ count-index.py
@@ -0,0 +1,17 @@
+s="I like python programming"
+print(s.count('p'))
+print(s.count('i'))
+#we will now look at some of the exceptional problems
+front=['html',5,'css',10,'javascript','react','photoshop',5,'html',9,'css','ui','html',10,'react']
+print(front.count('html'))
+print(front.count('react'))
+print(front.count('css'))
+#now we will see what happens in case of double quatation 
+print(front.count("html"))
+#Now we will see what happens in case of single letter
+print(front.count('h'))
+#Now we will see what is the integer number in case of quotation
+print(front.count('5'))
+#Now let's see what happens without quatation
+print(front.count(5))
+#Solve more new problems.Best of luck
