e870b49392f02d9b35f3139b7939c6fe9674f918
EIkhan <ebrahimkhan5222@gmail.com>
2020-05-09 03:08:49 +0600

    Added splitting

---
+++ splitting.py
@@ -0,0 +1,15 @@
+#splitting and joining strings
+#Now we will see splitting strings
+Shakib='shakib al hasan is the best all-rounder in the world'
+hasan=Shakib.split()
+print(hasan)
+#Now we will see
+hasan=Shakib.split('ha')
+print(hasan)
+#Now we will see joining strings
+Shakib=['bat','ball','gloves','run','wicket']
+al =  ' , '
+hasan=al.join(Shakib)
+print(al)
+print(Shakib)
+
