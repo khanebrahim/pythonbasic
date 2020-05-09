890e13e08263c3a4eca990b6b74ac58f5a3dd25f
EIkhan <ebrahimkhan5222@gmail.com>
2020-05-09 17:28:05 +0600

    Added forloop

--- Concatention.py
+++ Concatention.py
@@ -1,4 +1,5 @@
 sports=['cricket','football','tennis','basketball','golf']
 #print([1,2]+[2,4])
 print(sports+[12,13,6,8])
+#try more 
 #print((sports+[2])*4)
---
+++ forloop.py
@@ -0,0 +1,26 @@
+for name in ["shakib","tamim","mashrafee","mushfiqur","rubel","mahmudullah"]:
+    print("hi",name,"you are play in the next match") 
+
+# Now we will see
+
+for villiers in "best batting performance":
+    print(villiers)    
+
+#how many times is the word LM10    
+M="great messi"
+for L in M:
+    print("LM10")
+
+#turtle
+import turtle
+git=turtle.Screen()
+git.bgcolor('lightgreen')     
+github=turtle.Turtle()
+github.pensize(5)
+
+for gColor in ["yellow","red","purple","blue"]:    
+    github.color(gColor)
+    github.forward(180)
+    github.right(90)
+git.exitonclick()
+
